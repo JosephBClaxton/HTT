c9e1ef5e794933e03009d1c6cbc0770e7b3f423c
JosephBClaxton <joephone375@gmail.com>
2016-12-18 12:52:06 +0000

    FactTile and DDWFT
    The core contract on the Public BlockChain is that between a FactTile and the Decentralised Data Warehouse Fact Table (DDWFT)

---
+++ HTT_DDWFT_RECEIVER.sol
@@ -0,0 +1,16 @@
+/// HTT Hackerthon Draft Decentralised Data Warehouse Fact Table (DDWFT)
+/// DDWFT is the RECEIVER / BUYER and receives  
+/// In cases where the contract request is rejected a rejection code is noted in the DDWFT Log.
+/// Description
+/// The Decentralised Data Warehouse Fact Table  (DDWFT) lives on the public live BlockChain.
+/// Individual SME and Corporate Customers will be as coded as private chains / coloured coins
+/// including data put in the public domain. Their details will be received into the DDWFT.
+/// Note:
+/// In addition to handling FTC contracts the DDWFT also receives gifted or traded tokens that
+/// altruistic or profit minded users can earn from their own tiles and dispose of as they see fit.
+/// In contract terminology the DDWFT is the buyer or recipient and the Factile is the sender or seller. 
+/// 
+/// Change Control Notes
+/// Author	Date 		Version 	Reason For Change
+/// JBC		18/12/2016  0.1 		Original
+
---
+++ HTT_FactTile_SENDER.sol
@@ -0,0 +1,15 @@
+/// HTT Hackerthon Draft FactTile Code (FTC)
+/// FTC = SENDER / SELLER and has a contract with the DDWFT such that the DDWFT is the RECEIVER / BUYER
+/// In cases where the contract request is rejected a rejection code is noted in the FTC Log.
+/// Description
+/// FactTiles such as this make up the Decentralised Data Warehouse Fact Table  (DDWFT)
+/// living on the public live BlockChain.
+/// Individual SME and Corporate Customers will be as coded as private chains / coloured coins
+/// including data put in the public domain. Their 'links' will be coded as a DDWFT.
+/// 
+/// In contract terminology the Factile is the sender or seller. The DDWFT is the buyer or recipient.
+/// 
+/// Change Control Notes
+/// Author	Date 		Version 	Reason For Change
+/// JBC		18/12/2016  0.1 		Original
+
